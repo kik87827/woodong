7a696b79b975b3a65756203ac4ee3217ab39bc80
김인경 <kik87827@gmail.com>
2022-12-18 13:32:42 +0900

    진행중42

--- dist/assets/js/front.js
+++ dist/assets/js/front.js
@@ -652,3 +652,29 @@ function topBannerFunc() {
     });
   }
 }
+
+
+function getNextSiblings(elem, filter) {
+  var sibs = [];
+  var nextElem = elem.parentNode.firstChild;
+  do {
+    if (nextElem.nodeType === 3) continue; // ignore text nodes
+    if (nextElem === elem) continue; // ignore elem of target
+    if (nextElem === elem.nextElementSibling) {
+      if (!filter || filter(elem)) {
+        sibs.push(nextElem);
+        elem = nextElem;
+      }
+    }
+  } while (nextElem = nextElem.nextSibling)
+  return sibs;
+}
+
+function getPreviousSiblings(elem, filter) {
+  var sibs = [];
+  while (elem = elem.previousSibling) {
+    if (elem.nodeType === 3) continue; // ignore text nodes
+    if (!filter || filter(elem)) sibs.push(elem);
+  }
+  return sibs;
+}
